eb3d1d230bb62de8af639cb14659fbc3c3fb1132
Thomas Rose <thomas.rose@daad-alumni.de>
2024-09-07 22:48:39 +0200

    create abstract repository, link abstracts for 2024

---
+++ assets/abstract_processing.R
@@ -0,0 +1,92 @@
+# Function to programmatically turn abstracts in table into single quarto markdown documents for listings page 
+
+# import abstract data from GoogleSheets and clean-up table
+
+library(googlesheets4)
+
+date <- "2024" # year of workshop
+
+data <- read_sheet("1tJiRxF3S4m1ib-JWOKKnzenJC9RPUIlm-Z9FB986kIw", range = "E:BL", col_types = "c")
+
+data <- subset(
+  data, select = c(
+    "Running_order",
+    "Email Address", 
+    "Title of abstract", 
+    "Keywords", 
+    "Abstract", 
+    grep("^First", names(data), value = TRUE), 
+    grep("^Affiliation", names(data), value = TRUE), 
+    grep("^ORCID", names(data), value = TRUE), 
+    grep("^Institution", names(data), value = TRUE)
+  )
+)
+
+names(data) <- c("Running_order", "Mail", "Title", "Keywords", "Abstract", paste0("Author_", 1:10), paste0("Affiliation_", 1:10), paste0("ORCID_", 1:10), paste0("Institution_", 1:7))
+
+data$Keywords <- gsub(";", ",", data$Keywords)
+
+# create article
+
+for (i in 1:nrow(data)) {
+  
+  cat(
+    '---', 
+    paste0("title: '", data$Title[i], "'"), 
+    paste0('date: "', date, '"'), 
+    'date-format: "YYYY"',
+    paste0('categories: [', data$Keywords[i], ']'), 
+    
+    # Author 1
+    'author: ',
+    paste0('  - name: ', data$Author_1[i]), 
+    if (!is.na(data$ORCID_1[i])) {paste0('    orcid: ', data$ORCID_1[i])},
+    '    corresponding: true', 
+    paste0('    email: ', data$Mail[i]), 
+    
+    # Author 2
+    if (!is.na(data$Author_2[i])) {paste0('  - name: ', data$Author_2[i])},
+    if (!is.na(data$ORCID_2[i])) {paste0('    orcid: ', data$ORCID_2[i])}, 
+    
+    # Author 3
+    if (!is.na(data$Author_3[i])) {paste0('  - name: ', data$Author_3[i])},
+    if (!is.na(data$ORCID_3[i])) {paste0('    orcid: ', data$ORCID_3[i])}, 
+    
+    # Author 4
+    if (!is.na(data$Author_4[i])) {paste0('  - name: ', data$Author_4[i])},
+    if (!is.na(data$ORCID_4[i])) {paste0('    orcid: ', data$ORCID_4[i])}, 
+    
+    # Author 5
+    if (!is.na(data$Author_5[i])) {paste0('  - name: ', data$Author_5[i])},
+    if (!is.na(data$ORCID_5[i])) {paste0('    orcid: ', data$ORCID_5[i])}, 
+    
+    # Author 6
+    if (!is.na(data$Author_6[i])) {paste0('  - name: ', data$Author_6[i])},
+    if (!is.na(data$ORCID_6[i])) {paste0('    orcid: ', data$ORCID_6[i])}, 
+    
+    # Author 7
+    if (!is.na(data$Author_7[i])) {paste0('  - name: ', data$Author_7[i])},
+    if (!is.na(data$ORCID_7[i])) {paste0('    orcid: ', data$ORCID_7[i])}, 
+    
+    # Author 8
+    if (!is.na(data$Author_8[i])) {paste0('  - name: ', data$Author_8[i])},
+    if (!is.na(data$ORCID_8[i])) {paste0('    orcid: ', data$ORCID_8[i])}, 
+    
+    # Author 9
+    if (!is.na(data$Author_9[i])) {paste0('  - name: ', data$Author_9[i])},
+    if (!is.na(data$ORCID_9[i])) {paste0('    orcid: ', data$ORCID_9[i])}, 
+    
+    # Author 10
+    if (!is.na(data$Author_10[i])) {paste0('  - name: ', data$Author_10[i])},
+    if (!is.na(data$ORCID_10[i])) {paste0('    orcid: ', data$ORCID_10[i])}, 
+    
+    '---', 
+    '', 
+    data$Abstract[i], 
+    '', 
+    '[![CC-BY 4.0 icon](https://i.creativecommons.org/l/by/4.0/88x31.png)](http://creativecommons.org/licenses/by/4.0/)&nbsp;&nbsp; This work is licensed under a [Creative Commons Attribution 4.0 International License](https://creativecommons.org/licenses/by/4.0/). ',
+    sep = "\n", 
+    file = paste0('abstracts/', date, "-", data$Running_order[i], '.qmd')
+  )
+}
+
